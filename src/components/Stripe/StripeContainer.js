24a5b98595335ee8c9a842ae2d61a3fa6f4b1c5f
OJane1 <60651332+OJane1@users.noreply.github.com>
2025-04-14 12:17:18 +0200

    Add files via upload

---
+++ src/components/Stripe/StripeContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { CheckoutForm } from "./CheckoutForm";
+
+const PUBLIC_KEY = "pk_test_51Q3aYW08NEnQxjUuWNqqtgJdceGrQ8BYVjBL5oWhGPP4JUxS58WW3DI0RhNQHfVnQGyxQ3P0soUK6zyeGIVZh8a1003X75t65Q";
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+const StripeContainer = ({totalPrice, onClose, onSuccess}) => {
+  return (
+    <Elements stripe={stripeTestPromise}>
+      <CheckoutForm 
+        totalPrice={totalPrice} 
+        onClose={onClose}
+        onSuccess={onSuccess}
+      />
+    </Elements>
+  );
+};
+
+export default StripeContainer;
